c6baa94c0adf739b9719017d78368b54a77e5c8b
gogrimanan <manan@manans-macbook-pro.local>
2016-02-27 21:29:03 -0500

    Completed part4

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Manan Gogri on 2/27/16.
+//  Copyright © 2016 Manan Gogri. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+typealias JSONArray = Array<AnyObject>
